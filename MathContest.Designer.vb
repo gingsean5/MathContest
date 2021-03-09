123f5a4fa1bf95c540c25773c0d420f556a94242
FroYoMan <34078793+FroYoMan@users.noreply.github.com>
2021-03-09 08:32:53 -0700

    Add project files.

---
+++ MathContest.Designer.vb
@@ -0,0 +1,363 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class MathContest
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.NameTextBox = New System.Windows.Forms.TextBox()
+        Me.NameLabel = New System.Windows.Forms.Label()
+        Me.StudentInfoGroupBox = New System.Windows.Forms.GroupBox()
+        Me.GradeLabel = New System.Windows.Forms.Label()
+        Me.AgeLabel = New System.Windows.Forms.Label()
+        Me.GradeTextBox = New System.Windows.Forms.TextBox()
+        Me.AgeTextBox = New System.Windows.Forms.TextBox()
+        Me.ButtonGroupBox = New System.Windows.Forms.GroupBox()
+        Me.ExitButton = New System.Windows.Forms.Button()
+        Me.SummaryButton = New System.Windows.Forms.Button()
+        Me.ClearButton = New System.Windows.Forms.Button()
+        Me.SubmitButton = New System.Windows.Forms.Button()
+        Me.MathProblemGroupBox = New System.Windows.Forms.GroupBox()
+        Me.StudentAnswerLabel = New System.Windows.Forms.Label()
+        Me.SecondNumLabel = New System.Windows.Forms.Label()
+        Me.FirstNumLabel = New System.Windows.Forms.Label()
+        Me.StudentAnswerTextBox = New System.Windows.Forms.TextBox()
+        Me.SecondNumTextBox = New System.Windows.Forms.TextBox()
+        Me.FirstNumTextBox = New System.Windows.Forms.TextBox()
+        Me.ProblemTypeGroupBox = New System.Windows.Forms.GroupBox()
+        Me.DivideRadioButton = New System.Windows.Forms.RadioButton()
+        Me.MultiplyRadioButton = New System.Windows.Forms.RadioButton()
+        Me.SubtractRadioButton = New System.Windows.Forms.RadioButton()
+        Me.AddRadioButton = New System.Windows.Forms.RadioButton()
+        Me.MathContestToolTip = New System.Windows.Forms.ToolTip(Me.components)
+        Me.StudentInfoGroupBox.SuspendLayout()
+        Me.ButtonGroupBox.SuspendLayout()
+        Me.MathProblemGroupBox.SuspendLayout()
+        Me.ProblemTypeGroupBox.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'NameTextBox
+        '
+        Me.NameTextBox.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.NameTextBox.Location = New System.Drawing.Point(22, 59)
+        Me.NameTextBox.Name = "NameTextBox"
+        Me.NameTextBox.Size = New System.Drawing.Size(223, 30)
+        Me.NameTextBox.TabIndex = 0
+        Me.MathContestToolTip.SetToolTip(Me.NameTextBox, "Student's Name")
+        '
+        'NameLabel
+        '
+        Me.NameLabel.AutoSize = True
+        Me.NameLabel.Location = New System.Drawing.Point(19, 39)
+        Me.NameLabel.Name = "NameLabel"
+        Me.NameLabel.Size = New System.Drawing.Size(45, 17)
+        Me.NameLabel.TabIndex = 1
+        Me.NameLabel.Text = "Name"
+        '
+        'StudentInfoGroupBox
+        '
+        Me.StudentInfoGroupBox.Controls.Add(Me.GradeLabel)
+        Me.StudentInfoGroupBox.Controls.Add(Me.AgeLabel)
+        Me.StudentInfoGroupBox.Controls.Add(Me.GradeTextBox)
+        Me.StudentInfoGroupBox.Controls.Add(Me.AgeTextBox)
+        Me.StudentInfoGroupBox.Controls.Add(Me.NameLabel)
+        Me.StudentInfoGroupBox.Controls.Add(Me.NameTextBox)
+        Me.StudentInfoGroupBox.Location = New System.Drawing.Point(41, 35)
+        Me.StudentInfoGroupBox.Name = "StudentInfoGroupBox"
+        Me.StudentInfoGroupBox.Size = New System.Drawing.Size(493, 106)
+        Me.StudentInfoGroupBox.TabIndex = 0
+        Me.StudentInfoGroupBox.TabStop = False
+        Me.StudentInfoGroupBox.Text = "Student Information"
+        '
+        'GradeLabel
+        '
+        Me.GradeLabel.AutoSize = True
+        Me.GradeLabel.Location = New System.Drawing.Point(402, 33)
+        Me.GradeLabel.Name = "GradeLabel"
+        Me.GradeLabel.Size = New System.Drawing.Size(48, 17)
+        Me.GradeLabel.TabIndex = 6
+        Me.GradeLabel.Text = "Grade"
+        '
+        'AgeLabel
+        '
+        Me.AgeLabel.AutoSize = True
+        Me.AgeLabel.Location = New System.Drawing.Point(331, 33)
+        Me.AgeLabel.Name = "AgeLabel"
+        Me.AgeLabel.Size = New System.Drawing.Size(33, 17)
+        Me.AgeLabel.TabIndex = 5
+        Me.AgeLabel.Text = "Age"
+        '
+        'GradeTextBox
+        '
+        Me.GradeTextBox.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.GradeTextBox.Location = New System.Drawing.Point(405, 53)
+        Me.GradeTextBox.Name = "GradeTextBox"
+        Me.GradeTextBox.Size = New System.Drawing.Size(30, 30)
+        Me.GradeTextBox.TabIndex = 2
+        Me.GradeTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        Me.MathContestToolTip.SetToolTip(Me.GradeTextBox, "Student must be between 1st and 4th grade")
+        '
+        'AgeTextBox
+        '
+        Me.AgeTextBox.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.AgeTextBox.Location = New System.Drawing.Point(334, 53)
+        Me.AgeTextBox.Name = "AgeTextBox"
+        Me.AgeTextBox.Size = New System.Drawing.Size(30, 30)
+        Me.AgeTextBox.TabIndex = 1
+        Me.AgeTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        Me.MathContestToolTip.SetToolTip(Me.AgeTextBox, "Student must be 7 to 11 years old")
+        '
+        'ButtonGroupBox
+        '
+        Me.ButtonGroupBox.Controls.Add(Me.ExitButton)
+        Me.ButtonGroupBox.Controls.Add(Me.SummaryButton)
+        Me.ButtonGroupBox.Controls.Add(Me.ClearButton)
+        Me.ButtonGroupBox.Controls.Add(Me.SubmitButton)
+        Me.ButtonGroupBox.Location = New System.Drawing.Point(561, 35)
+        Me.ButtonGroupBox.Name = "ButtonGroupBox"
+        Me.ButtonGroupBox.Size = New System.Drawing.Size(228, 399)
+        Me.ButtonGroupBox.TabIndex = 3
+        Me.ButtonGroupBox.TabStop = False
+        '
+        'ExitButton
+        '
+        Me.ExitButton.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.ExitButton.Location = New System.Drawing.Point(6, 297)
+        Me.ExitButton.Name = "ExitButton"
+        Me.ExitButton.Size = New System.Drawing.Size(222, 83)
+        Me.ExitButton.TabIndex = 9
+        Me.ExitButton.Text = "E&xit"
+        Me.MathContestToolTip.SetToolTip(Me.ExitButton, "End the program")
+        Me.ExitButton.UseVisualStyleBackColor = True
+        '
+        'SummaryButton
+        '
+        Me.SummaryButton.Location = New System.Drawing.Point(6, 208)
+        Me.SummaryButton.Name = "SummaryButton"
+        Me.SummaryButton.Size = New System.Drawing.Size(222, 83)
+        Me.SummaryButton.TabIndex = 8
+        Me.SummaryButton.Text = "Su&mmary"
+        Me.SummaryButton.UseVisualStyleBackColor = True
+        '
+        'ClearButton
+        '
+        Me.ClearButton.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.ClearButton.Location = New System.Drawing.Point(5, 119)
+        Me.ClearButton.Name = "ClearButton"
+        Me.ClearButton.Size = New System.Drawing.Size(222, 83)
+        Me.ClearButton.TabIndex = 1
+        Me.ClearButton.TabStop = False
+        Me.ClearButton.Text = "&Clear"
+        Me.MathContestToolTip.SetToolTip(Me.ClearButton, "Clear all text from the form (Esc key)")
+        Me.ClearButton.UseVisualStyleBackColor = True
+        '
+        'SubmitButton
+        '
+        Me.SubmitButton.Location = New System.Drawing.Point(5, 30)
+        Me.SubmitButton.Name = "SubmitButton"
+        Me.SubmitButton.Size = New System.Drawing.Size(222, 83)
+        Me.SubmitButton.TabIndex = 7
+        Me.SubmitButton.Text = "&Submit"
+        Me.MathContestToolTip.SetToolTip(Me.SubmitButton, "Check answer (Enter key)")
+        Me.SubmitButton.UseVisualStyleBackColor = True
+        '
+        'MathProblemGroupBox
+        '
+        Me.MathProblemGroupBox.Controls.Add(Me.StudentAnswerLabel)
+        Me.MathProblemGroupBox.Controls.Add(Me.SecondNumLabel)
+        Me.MathProblemGroupBox.Controls.Add(Me.FirstNumLabel)
+        Me.MathProblemGroupBox.Controls.Add(Me.StudentAnswerTextBox)
+        Me.MathProblemGroupBox.Controls.Add(Me.SecondNumTextBox)
+        Me.MathProblemGroupBox.Controls.Add(Me.FirstNumTextBox)
+        Me.MathProblemGroupBox.Location = New System.Drawing.Point(41, 169)
+        Me.MathProblemGroupBox.Name = "MathProblemGroupBox"
+        Me.MathProblemGroupBox.Size = New System.Drawing.Size(267, 246)
+        Me.MathProblemGroupBox.TabIndex = 1
+        Me.MathProblemGroupBox.TabStop = False
+        Me.MathProblemGroupBox.Text = "Current Math Problem"
+        '
+        'StudentAnswerLabel
+        '
+        Me.StudentAnswerLabel.AutoSize = True
+        Me.StudentAnswerLabel.Location = New System.Drawing.Point(19, 166)
+        Me.StudentAnswerLabel.Name = "StudentAnswerLabel"
+        Me.StudentAnswerLabel.Size = New System.Drawing.Size(107, 17)
+        Me.StudentAnswerLabel.TabIndex = 11
+        Me.StudentAnswerLabel.Text = "Student Answer"
+        '
+        'SecondNumLabel
+        '
+        Me.SecondNumLabel.AutoSize = True
+        Me.SecondNumLabel.Location = New System.Drawing.Point(19, 97)
+        Me.SecondNumLabel.Name = "SecondNumLabel"
+        Me.SecondNumLabel.Size = New System.Drawing.Size(86, 17)
+        Me.SecondNumLabel.TabIndex = 10
+        Me.SecondNumLabel.Text = "2nd Number"
+        '
+        'FirstNumLabel
+        '
+        Me.FirstNumLabel.AutoSize = True
+        Me.FirstNumLabel.Location = New System.Drawing.Point(19, 27)
+        Me.FirstNumLabel.Name = "FirstNumLabel"
+        Me.FirstNumLabel.Size = New System.Drawing.Size(81, 17)
+        Me.FirstNumLabel.TabIndex = 7
+        Me.FirstNumLabel.Text = "1st Number"
+        '
+        'StudentAnswerTextBox
+        '
+        Me.StudentAnswerTextBox.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.StudentAnswerTextBox.Location = New System.Drawing.Point(22, 186)
+        Me.StudentAnswerTextBox.Name = "StudentAnswerTextBox"
+        Me.StudentAnswerTextBox.Size = New System.Drawing.Size(223, 30)
+        Me.StudentAnswerTextBox.TabIndex = 5
+        Me.MathContestToolTip.SetToolTip(Me.StudentAnswerTextBox, "Student answer for the math problem")
+        '
+        'SecondNumTextBox
+        '
+        Me.SecondNumTextBox.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.SecondNumTextBox.Location = New System.Drawing.Point(22, 116)
+        Me.SecondNumTextBox.Name = "SecondNumTextBox"
+        Me.SecondNumTextBox.Size = New System.Drawing.Size(223, 30)
+        Me.SecondNumTextBox.TabIndex = 4
+        Me.MathContestToolTip.SetToolTip(Me.SecondNumTextBox, "2nd number of the math problem")
+        '
+        'FirstNumTextBox
+        '
+        Me.FirstNumTextBox.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.FirstNumTextBox.Location = New System.Drawing.Point(22, 47)
+        Me.FirstNumTextBox.Name = "FirstNumTextBox"
+        Me.FirstNumTextBox.Size = New System.Drawing.Size(223, 30)
+        Me.FirstNumTextBox.TabIndex = 3
+        Me.MathContestToolTip.SetToolTip(Me.FirstNumTextBox, "1st number of the math problem")
+        '
+        'ProblemTypeGroupBox
+        '
+        Me.ProblemTypeGroupBox.Controls.Add(Me.DivideRadioButton)
+        Me.ProblemTypeGroupBox.Controls.Add(Me.MultiplyRadioButton)
+        Me.ProblemTypeGroupBox.Controls.Add(Me.SubtractRadioButton)
+        Me.ProblemTypeGroupBox.Controls.Add(Me.AddRadioButton)
+        Me.ProblemTypeGroupBox.Location = New System.Drawing.Point(337, 169)
+        Me.ProblemTypeGroupBox.Name = "ProblemTypeGroupBox"
+        Me.ProblemTypeGroupBox.Size = New System.Drawing.Size(197, 245)
+        Me.ProblemTypeGroupBox.TabIndex = 2
+        Me.ProblemTypeGroupBox.TabStop = False
+        Me.ProblemTypeGroupBox.Text = "Math Problem Type"
+        '
+        'DivideRadioButton
+        '
+        Me.DivideRadioButton.AutoSize = True
+        Me.DivideRadioButton.Location = New System.Drawing.Point(58, 161)
+        Me.DivideRadioButton.Name = "DivideRadioButton"
+        Me.DivideRadioButton.Size = New System.Drawing.Size(68, 21)
+        Me.DivideRadioButton.TabIndex = 3
+        Me.DivideRadioButton.TabStop = True
+        Me.DivideRadioButton.Text = "Divide"
+        Me.MathContestToolTip.SetToolTip(Me.DivideRadioButton, "1st Number / 2nd Number")
+        Me.DivideRadioButton.UseVisualStyleBackColor = True
+        '
+        'MultiplyRadioButton
+        '
+        Me.MultiplyRadioButton.AutoSize = True
+        Me.MultiplyRadioButton.Location = New System.Drawing.Point(58, 125)
+        Me.MultiplyRadioButton.Name = "MultiplyRadioButton"
+        Me.MultiplyRadioButton.Size = New System.Drawing.Size(76, 21)
+        Me.MultiplyRadioButton.TabIndex = 2
+        Me.MultiplyRadioButton.TabStop = True
+        Me.MultiplyRadioButton.Text = "Multiply"
+        Me.MathContestToolTip.SetToolTip(Me.MultiplyRadioButton, "1st Number * 2nd Number")
+        Me.MultiplyRadioButton.UseVisualStyleBackColor = True
+        '
+        'SubtractRadioButton
+        '
+        Me.SubtractRadioButton.AutoSize = True
+        Me.SubtractRadioButton.Location = New System.Drawing.Point(58, 91)
+        Me.SubtractRadioButton.Name = "SubtractRadioButton"
+        Me.SubtractRadioButton.Size = New System.Drawing.Size(82, 21)
+        Me.SubtractRadioButton.TabIndex = 1
+        Me.SubtractRadioButton.TabStop = True
+        Me.SubtractRadioButton.Text = "Subtract"
+        Me.MathContestToolTip.SetToolTip(Me.SubtractRadioButton, "1st Number - 2nd Number")
+        Me.SubtractRadioButton.UseVisualStyleBackColor = True
+        '
+        'AddRadioButton
+        '
+        Me.AddRadioButton.AutoSize = True
+        Me.AddRadioButton.Location = New System.Drawing.Point(58, 55)
+        Me.AddRadioButton.Name = "AddRadioButton"
+        Me.AddRadioButton.Size = New System.Drawing.Size(54, 21)
+        Me.AddRadioButton.TabIndex = 0
+        Me.AddRadioButton.TabStop = True
+        Me.AddRadioButton.Text = "Add"
+        Me.MathContestToolTip.SetToolTip(Me.AddRadioButton, "1st Number + 2nd Number")
+        Me.AddRadioButton.UseVisualStyleBackColor = True
+        '
+        'MathContest
+        '
+        Me.AcceptButton = Me.SubmitButton
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.CancelButton = Me.ClearButton
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.ProblemTypeGroupBox)
+        Me.Controls.Add(Me.MathProblemGroupBox)
+        Me.Controls.Add(Me.ButtonGroupBox)
+        Me.Controls.Add(Me.StudentInfoGroupBox)
+        Me.MinimumSize = New System.Drawing.Size(100, 100)
+        Me.Name = "MathContest"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Math Contest"
+        Me.StudentInfoGroupBox.ResumeLayout(False)
+        Me.StudentInfoGroupBox.PerformLayout()
+        Me.ButtonGroupBox.ResumeLayout(False)
+        Me.MathProblemGroupBox.ResumeLayout(False)
+        Me.MathProblemGroupBox.PerformLayout()
+        Me.ProblemTypeGroupBox.ResumeLayout(False)
+        Me.ProblemTypeGroupBox.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents NameTextBox As TextBox
+    Friend WithEvents NameLabel As Label
+    Friend WithEvents StudentInfoGroupBox As GroupBox
+    Friend WithEvents GradeLabel As Label
+    Friend WithEvents AgeLabel As Label
+    Friend WithEvents GradeTextBox As TextBox
+    Friend WithEvents AgeTextBox As TextBox
+    Friend WithEvents ButtonGroupBox As GroupBox
+    Friend WithEvents ExitButton As Button
+    Friend WithEvents SummaryButton As Button
+    Friend WithEvents ClearButton As Button
+    Friend WithEvents SubmitButton As Button
+    Friend WithEvents MathProblemGroupBox As GroupBox
+    Friend WithEvents StudentAnswerLabel As Label
+    Friend WithEvents SecondNumLabel As Label
+    Friend WithEvents FirstNumLabel As Label
+    Friend WithEvents StudentAnswerTextBox As TextBox
+    Friend WithEvents SecondNumTextBox As TextBox
+    Friend WithEvents FirstNumTextBox As TextBox
+    Friend WithEvents ProblemTypeGroupBox As GroupBox
+    Friend WithEvents DivideRadioButton As RadioButton
+    Friend WithEvents MultiplyRadioButton As RadioButton
+    Friend WithEvents SubtractRadioButton As RadioButton
+    Friend WithEvents AddRadioButton As RadioButton
+    Friend WithEvents MathContestToolTip As ToolTip
+End Class
---
+++ MathContest.vb
@@ -0,0 +1,170 @@
+﻿Public Class MathContest
+    Function ValidateFields() As Boolean
+        Dim StudentAge As Integer
+        Dim StudentGrade As Integer
+        Dim ProblemMessage As String
+        Dim StudentName As String
+        StudentName = CStr(NameTextBox.Text)
+
+
+        Dim CorrectAnswer As Integer
+        Dim StudentAnswer As Integer
+
+        Try
+            StudentAnswer = CInt(StudentAnswerTextBox.Text)
+        Catch ex As Exception
+            ProblemMessage &= "The student answer must be a number" & vbNewLine
+            StudentAnswerTextBox.Text = ""
+            StudentAnswerTextBox.Focus()
+        End Try
+
+        If AddRadioButton.Checked Then
+            CorrectAnswer = MathTime(0)
+        End If
+        If SubtractRadioButton.Checked Then
+            CorrectAnswer = MathTime(1)
+        End If
+        If MultiplyRadioButton.Checked Then
+            CorrectAnswer = MathTime(2)
+        End If
+        If DivideRadioButton.Checked Then
+            CorrectAnswer = MathTime(3)
+        End If
+
+        Dim FirstNum As Integer
+        Dim SecondNum As Integer
+        Try
+            SecondNum = CInt(SecondNumTextBox.Text)
+        Catch ex As Exception
+            ProblemMessage &= "The 2nd number must be a number"
+            SecondNumTextBox.Text = ""
+        End Try
+        If SecondNumTextBox.Text = "" Then
+            SecondNumTextBox.Focus()
+        End If
+        Try
+            FirstNum = CInt(FirstNumTextBox.Text)
+        Catch ex As Exception
+            ProblemMessage &= "The 1st number must be a number"
+            FirstNumTextBox.Text = ""
+        End Try
+        If FirstNumTextBox.Text = "" Then
+            FirstNumTextBox.Focus()
+        End If
+
+        Try
+            StudentGrade = CInt(GradeTextBox.Text)
+        Catch ex As Exception
+            ProblemMessage &= "Grade must be a number between 1 and 4" & vbNewLine
+        End Try
+
+        If StudentGrade > 4 Then
+            ProblemMessage &= $"{StudentName} is too far along in school to compete" & vbNewLine
+            GradeTextBox.Text = ""
+            GradeTextBox.Focus()
+        End If
+        If StudentGrade < 1 Then
+            ProblemMessage &= $"{StudentName} has not yet had enough schooling to compete" & vbNewLine
+            GradeTextBox.Text = ""
+            GradeTextBox.Focus()
+        End If
+
+        Try
+            StudentAge = CInt(AgeTextBox.Text)
+        Catch ex As Exception
+            ProblemMessage &= "Age must be a number between 7 and 11" & vbNewLine
+        End Try
+
+        If StudentAge > 11 Then
+            ProblemMessage &= $"{StudentName} is too old to compete" & vbNewLine
+            AgeTextBox.Text = ""
+            AgeTextBox.Focus()
+        End If
+        If StudentAge < 7 Then
+            ProblemMessage &= $"{StudentName} is too young to compete" & vbNewLine
+            AgeTextBox.Text = ""
+            AgeTextBox.Focus()
+        End If
+
+        If NameTextBox.Text = "" Then
+            ProblemMessage &= "The student's name is required" & vbNewLine
+            NameTextBox.Focus()
+        End If
+
+        If ProblemMessage <> "" Then
+            MsgBox(ProblemMessage)
+        Else
+            If StudentAnswer = CorrectAnswer Then
+                MsgBox($"Correct! Well done {StudentName}" & vbNewLine)
+            Else
+                MsgBox($"Incorrect. Get it together {StudentName}. The correct answer is {CorrectAnswer}" & vbNewLine)
+            End If
+        End If
+
+    End Function
+
+    Function MathTime(ByRef MathType As Integer) As Integer
+        Dim FirstNum As Integer
+        Dim SecondNum As Integer
+        Dim CorrectAnswer As Integer
+        Try
+            FirstNum = CInt(FirstNumTextBox.Text)
+            SecondNum = CInt(SecondNumTextBox.Text)
+        Catch ex As Exception
+            MsgBox("Math can only be done with 2 numbers")
+        End Try
+        Select Case MathType
+            Case 0
+                CorrectAnswer = FirstNum + SecondNum
+            Case 1
+                CorrectAnswer = FirstNum - SecondNum
+            Case 2
+                CorrectAnswer = FirstNum * SecondNum
+            Case 3
+                CorrectAnswer = FirstNum / SecondNum
+        End Select
+        Return CorrectAnswer
+    End Function
+
+    Private Sub ExitButton_Click(sender As Object, e As EventArgs) Handles ExitButton.Click
+        Me.Close()
+    End Sub
+
+
+
+    Private Sub SubmitButton_Click(sender As Object, e As EventArgs) Handles SubmitButton.Click
+        ValidateFields()
+
+
+
+
+    End Sub
+
+    Private Sub ClearButton_Click(sender As Object, e As EventArgs) Handles ClearButton.Click
+        NameTextBox.Text = ""
+        AgeTextBox.Text = ""
+        GradeTextBox.Text = ""
+        FirstNumTextBox.Text = ""
+        SecondNumTextBox.Text = ""
+        StudentAnswerTextBox.Text = ""
+
+    End Sub
+
+    Private Sub AddRadioButton_CheckedChanged(sender As Object, e As EventArgs) Handles AddRadioButton.CheckedChanged
+        MathTime(0)
+    End Sub
+
+    Private Sub SubtractRadioButton_CheckedChanged(sender As Object, e As EventArgs) Handles SubtractRadioButton.CheckedChanged
+        MathTime(1)
+    End Sub
+
+    Private Sub MultiplyRadioButton_CheckedChanged(sender As Object, e As EventArgs) Handles MultiplyRadioButton.CheckedChanged
+        MathTime(2)
+    End Sub
+
+    Private Sub DivideRadioButton_CheckedChanged(sender As Object, e As EventArgs) Handles DivideRadioButton.CheckedChanged
+        MathTime(3)
+    End Sub
+
+
+End Class
